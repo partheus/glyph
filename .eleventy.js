1218beef9787b7ac2afc80932e7f7eaad24533f1
Parth <parth.shiralkar@gmail.com>
2023-10-24 10:52:57 -0700

    Add date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
 module.exports = function (eleventyConfig) {
+    // Add a collection of posts in reverse date order
     eleventyConfig.addCollection('post', function (collection) {
         return collection.getFilteredByGlob('src/posts/*.md').reverse();
+    });
+
+    // Create a custom "date" filter
+    eleventyConfig.addFilter('date', function(value, format) {
+        const date = new Date(value);
+        if (isNaN(date)) {
+            return value; // Return the original value if it's not a valid date
+        }
+        return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
     });
 
     eleventyConfig.addPassthroughCopy("src/css");
