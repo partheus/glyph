51df2a6a96bb8ccaeb5e4c787c88bf87412cd6fb
Parth <parth.shiralkar@gmail.com>
2023-10-24 01:08:55 -0700

    Add cname

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function (eleventyConfig) {
     });
 
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("CNAME");
+
     return {
         dir: {
             input: "src",
