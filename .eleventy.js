9c5360cea5d651bdf6d964427b7a442601203196
Parth <parth.shiralkar@gmail.com>
2023-10-25 16:35:29 -0700

    Add markdown support

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,24 @@
+const markdownIt = require("markdown-it");
+const markdownItFootnote = require("markdown-it-footnote");
+
 module.exports = function (eleventyConfig) {
     // Add a collection of posts in reverse date order
     eleventyConfig.addCollection('post', function (collection) {
         return collection.getFilteredByGlob('src/posts/*.md').reverse();
     });
+
+    // Set template formats
+    eleventyConfig.setTemplateFormats(["njk", "md", "html"]);
+
+    // // Add support for footnotes
+    eleventyConfig.setLibrary("md", markdownIt(
+        {
+            html: true, // Enable HTML tags in source
+            breaks: true,  // Convert '\n' in paragraphs into <br>
+            linkify: true // Autoconvert URL-like text to links
+        }
+    ).use(markdownItFootnote));
+
 
     // Create a custom "date" filter
     eleventyConfig.addFilter('date', function(value, format) {
