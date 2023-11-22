6114e083cc34320bc192fcf6981ff211dbd86c02
Parth <parth.shiralkar@gmail.com>
2023-11-22 19:39:21 +0530

    Push favicon

--- .eleventy.js
+++ .eleventy.js
@@ -21,12 +21,20 @@ module.exports = function (eleventyConfig) {
 
 
     // Create a custom "date" filter
-    eleventyConfig.addFilter('date', function(value, format) {
+    eleventyConfig.addFilter('date', function (value, format) {
         const date = new Date(value);
         if (isNaN(date)) {
             return value; // Return the original value if it's not a valid date
         }
-        return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+        return date.toLocaleDateString(undefined, {year: 'numeric', month: 'long', day: 'numeric'});
+    });
+
+    // Create a slug from a string
+    eleventyConfig.addFilter('customPermalink', (page) => {
+        const year = page.date ? page.date.getFullYear() : new Date().getFullYear();
+        const fileSlug = page.fileSlug || '';
+
+        return `/${year}/posts/${fileSlug}/`;
     });
 
     eleventyConfig.addPassthroughCopy("src/css");
@@ -34,6 +42,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("public.asc");
     eleventyConfig.addPassthroughCopy(".nojekyll");
     eleventyConfig.addPassthroughCopy("garden-social.webp");
+    eleventyConfig.addPassthroughCopy({"favicon.ico": "/favicon.ico"});
 
     return {
         dir: {
